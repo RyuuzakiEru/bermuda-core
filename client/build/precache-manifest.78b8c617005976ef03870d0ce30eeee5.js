1770f0a10702cb4e08e5e58841e7364e72af147a
Roberto Morales <roberto.morales@comalatech.com>
2021-02-22 13:20:10 +0100

    update

---
+++ client/build/precache-manifest.78b8c617005976ef03870d0ce30eeee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d95ba7aec4bcf4ebe6242a33f0f2e54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "389a790ffd4579a1dd88",
+    "url": "./static/js/2.83f95064.chunk.js"
+  },
+  {
+    "revision": "40f0b2cf47eb6bff91f07a772f8cf383",
+    "url": "./static/js/2.83f95064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b65d0038675d17cc775",
+    "url": "./static/js/main.5ae1b699.chunk.js"
+  },
+  {
+    "revision": "607911425784431aa9f5",
+    "url": "./static/js/runtime-main.aef6d470.js"
+  }
+]);
